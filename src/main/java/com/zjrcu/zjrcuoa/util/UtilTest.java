cc1a7d437f2f6c7b54ae9c8b5db7a71d55811d3b
gorden <11879965282@qq.com>
2019-04-24 17:22:27 +0800

    hh

--- src/main/java/com/zjrcu/zjrcuoa/util/UtilTest.java
+++ src/main/java/com/zjrcu/zjrcuoa/util/UtilTest.java
@@ -1,5 +1,6 @@
 package com.zjrcu.zjrcuoa.util;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
@@ -22,5 +23,20 @@ public class UtilTest {
         Boolean b =  StringUtils.equals("",a);
         System.out.println(b); 
     }
+
+    @Test
+    public void stringEscapeUtilsTest() {
+
+            System.out.println("转义HTML,注意汉字:"+StringEscapeUtils.escapeHtml4("<font>chen磊  xing</font>"));    //转义HTML,注意汉字
+            System.out.println("反转义HTML:"+StringEscapeUtils.unescapeHtml4("&lt;font&gt;chen磊  xing&lt;/font&gt;"));  //反转义HTML
+
+            System.out.println("转成Unicode编码："+StringEscapeUtils.escapeJava("陈磊兴"));     //转义成Unicode编码
+
+            System.out.println("转义XML："+StringEscapeUtils.escapeXml("<name>陈磊兴</name>"));   //转义xml
+            System.out.println("反转义XML："+StringEscapeUtils.unescapeXml("<name>陈磊兴</name>"));    //转义xml
+
+        }
+
+
 }
 
